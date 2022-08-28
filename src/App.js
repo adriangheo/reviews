c0faa4d6635f42b0a77cdcc6d9a2493baeca0c35
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-08-28 14:58:33 +0300

    initial exercise setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
+import React from 'react';
+import Review from './Review';
 function App() {
-  return (
-    <div className="App">
-      Hello React!
-    </div>
-  );
+  return <h2>reviews project setup</h2>;
 }
 
 export default App;
