51af7cf6df48e5fcbe9ca05de5b40c5ee1e31d24
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-08-30 20:51:22 +0300

    final version of code

--- src/data.js
+++ src/data.js
@@ -1,39 +1,39 @@
 const reviews = [
   {
     id: 1,
-    name: 'susan smith',
-    job: 'web developer',
+    name: 'Leanne Graham',
+    job: 'front end developer',
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      'https://i.imgur.com/LaLaCEf.jpg',
     text:
-      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec augue arcu, tristique convallis lobortis sed, gravida et eros. Ut ac leo quis tellus vehicula interdum in nec sapien. Maecenas sed tortor lacus. Aenean lacus ipsum, tincidunt at molestie vitae, placerat sit amet elit. Integer sed eros non lorem pellentesque dictum vitae sed leo. ",
   },
   {
     id: 2,
-    name: 'anna johnson',
-    job: 'web designer',
+    name: 'Ervin Howell',
+    job: 'full stack designer',
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+      'https://i.imgur.com/pdHbz7N.jpg',
     text:
-      'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id nulla dolor. Vestibulum et laoreet sapien. Nulla facilisi. Proin hendrerit purus a porttitor maximus. Cras at pharetra tellus. Suspendisse mollis dapibus tellus. Curabitur a arcu lacinia, interdum orci eget, suscipit eros. Ut pretium pretium neque vitae fermentum. Morbi vel consectetur ex. Proin posuere odio et leo venenatis, vel dignissim diam rutrum. Duis et erat libero.',
   },
   {
     id: 3,
-    name: 'peter jones',
-    job: 'intern',
+    name: 'Clementine Bauch',
+    job: 'data scientist',
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+      'https://i.imgur.com/ZwZ0SNk.jpg',
     text:
-      'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam massa eros, blandit sit amet tristique fringilla, mattis ut ligula. Vivamus venenatis dolor est, vel elementum risus elementum ac. Praesent commodo augue et lorem scelerisque, id mattis orci tincidunt. Nam vel consectetur tortor. Integer a vulputate est. ',
   },
   {
     id: 4,
-    name: 'bill anderson',
-    job: 'the boss',
+    name: 'Patricia Lebsack',
+    job: 'graphical designer',
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+      'https://i.imgur.com/6QnI00J.jpg',
     text:
-      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ornare, nisi a congue finibus, libero massa mattis quam, at pretium tellus purus sed nunc. Vivamus sagittis auctor diam, sed imperdiet est pulvinar ut. Aliquam erat volutpat. Quisque ornare risus nisi. Pellentesque accumsan nisl sed dictum semper.',
   },
 ];
 
